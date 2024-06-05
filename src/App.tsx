d4eb7e07b3bfdbb6834d903c7828a10f9d156f3e
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-06-05 13:28:36 +0200

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import CreateCustomer from './components/CreateCustomer';
 import Customer from './components/Customer';
 
 const App = () => {
   return (
     <div>
       <Customer />
+      <CreateCustomer />
     </div>
   );
 };
---
+++ src/components/CreateCustomer.tsx
@@ -0,0 +1,20 @@
+const CreateCustomer = () => {
+  return (
+    <div>
+      <h2>Create</h2>
+      <form>
+        <label>
+          First name
+          <input type="text" />
+        </label>
+
+        <label>
+          Last name
+          <input type="text" />
+        </label>
+      </form>
+    </div>
+  );
+};
+
+export default CreateCustomer;
