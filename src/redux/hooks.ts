3a20a622f44365934cdf8da6433d93cc1b161480
Vitalii Voloshyn <voloshin.v.02@gmail.com>
2024-06-05 19:05:24 +0200

    define typed dispatch hook

--- src/redux/hooks.ts
+++ src/redux/hooks.ts
@@ -1,4 +1,5 @@
-import { useSelector } from 'react-redux';
-import { RootState } from './store';
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from '~/redux/store';
 
 export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -15,3 +15,4 @@ const composedEnhancer = composeWithDevTools(applyMiddleware(thunk));
 export const store = createStore(rootReducer, composedEnhancer);
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
